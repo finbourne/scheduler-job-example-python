daadbf1daac34fed87bf03947a9efb498b9a1e30
Stephen LeMasney <stephen.lemasney@finbourne.com>
2023-01-12 12:05:34 +0000

    SE-2293: check for image

---
+++ tests/test_image_vulnerabilities.py
@@ -0,0 +1,20 @@
+import unittest
+import lusid_scheduler as ls
+from fbnsdkutilities import ApiClientFactory
+
+class TestImageVulnerabilities(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.scheduler_factory = ApiClientFactory(ls)
+        cls.images_api = cls.scheduler_factory.build(ls.ImagesApi)
+        
+    def test_image_vulnerability(self):
+
+        image_name = "abor-data-loader:0.0.1"
+        test_image = self.images_api.get_image(name=image_name)
+        high_vulnerability = test_image.scan_report.summary.high
+        critical_vulnerability = test_image.scan_report.summary.critical
+
+        self.assertEqual(high_vulnerability, 0)
+        self.assertEqual(critical_vulnerability, 0)
